b972ed03f2ecc5a00639d9d251821bad39b463c3
YSRKEN <ysr.ken@gmail.com>
2018-07-10 22:36:18 +0900

    ng new OhanashiGen --routing --style=scss cd OhanashiGen ng add @angular/pwa --project=OhanashiGen ng g universal --client-project=OhanashiGen npm i ng g app-shell --client-project=OhanashiGen --universal-project=OhanashiGen

---
+++ OhanashiGen/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
