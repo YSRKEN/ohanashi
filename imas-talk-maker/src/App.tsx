efb8fc3ed197956eedd5b1c9f596851a4b8c8ea2
YSRKEN <ysr.ken@gmail.com>
2019-02-26 00:51:11 +0900

    App.tsxをReact.FCに変更した

--- imas-talk-maker/src/App.tsx
+++ imas-talk-maker/src/App.tsx
@@ -4,24 +4,20 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button onClick={
-          // tslint:disable-next-line: jsx-no-lambda
-          () => window.alert("Hello, React-Bootstrap!")
-        }>Push!</Button>
-      </div>
-    );
-  }
-}
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.tsx</code> and save to reload.
+    </p>
+    <Button onClick={
+      // tslint:disable-next-line: jsx-no-lambda
+      () => window.alert("Hello, React-Bootstrap!")
+    }>Push!</Button>
+  </div>
+);
 
 export default App;
