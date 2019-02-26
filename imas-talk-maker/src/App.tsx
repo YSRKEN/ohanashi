f3e6b71bb0435f83f5732b880060dae652e0ebac
YSRKEN <ysr.ken@gmail.com>
2019-02-26 22:38:14 +0900

    選択肢ボタンのコンポーネントを作成した

--- imas-talk-maker/src/App.tsx
+++ imas-talk-maker/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Button, Col, Container, FormGroup, Row } from 'react-bootstrap';
 import Form from 'react-bootstrap/FormGroup';
+import SelectButtonGroup from './SelectButtonGroup';
 
 const sampleFunction = () => window.alert("Hello, React-Bootstrap!");
 
@@ -9,6 +10,18 @@ const App: React.FC = () => (
     <Row>
       <Col>
         <Form>
+          <FormGroup>
+            <SelectButtonGroup nameList={['おはなし', 'デレぽ']}
+              firstSelectName='デレぽ' selectedColorType='primary' />
+          </FormGroup>
+          <FormGroup>
+            <SelectButtonGroup nameList={['デレマス', 'ミリマス', 'シャニマス']}
+              firstSelectName='ミリマス' selectedColorType='info' />
+          </FormGroup>
+          <FormGroup>
+            <SelectButtonGroup nameList={['まみみ', 'きりこ', 'こがね', 'ゆいか', 'さくや']}
+              firstSelectName='こがね' selectedColorType='danger' />
+          </FormGroup>
           <FormGroup>
             <Button onClick={sampleFunction}>Push!</Button>
           </FormGroup>
---
+++ imas-talk-maker/src/SelectButtonGroup.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+const SelectButtonGroup: React.FC<{
+	nameList: string[],			// 表示名一覧
+	firstSelectName: string,	// 最初に選択する名前
+	className?: string,			// ButtonGroupに設定するクラス
+	// 選択されている方のボタン色
+	selectedColorType?: 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
+}> = ({
+	nameList,
+	firstSelectName,
+	className,
+	selectedColorType = 'primary'
+}) => {
+	// 関数を定義する
+	const clickButtonFunc = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		// ボタンの名前を読み取る
+		const buttonName = e.currentTarget.textContent;
+		if (buttonName === null) {
+			return;
+		}
+
+		// 場合によって選択肢を切り替える
+		if (selectName === buttonName) {
+			return;
+		}
+		setSelectName(buttonName);
+	}
+
+	// Hooksを設定する
+	const [selectName, setSelectName] = React.useState(firstSelectName);
+
+	// レンダリングする
+	return (
+		<ButtonGroup className={className} role='group'>
+			{nameList.map((name, i) => {
+				if (name === selectName) {
+					return (
+						<Button className={`btn-${selectedColorType}`} key={i}
+							onClick={clickButtonFunc}>
+							{name}
+						</Button>
+					);
+				} else {
+					return (
+						<Button className="page-link text-dark d-inline-block" key={i}
+							onClick={clickButtonFunc}>
+							{name}
+						</Button>
+					);
+				}
+			})}
+		</ButtonGroup>
+	);
+}
+
+export default SelectButtonGroup;
