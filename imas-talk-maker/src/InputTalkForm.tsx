c552b457777dcb3fee9b22f933af509baf9ee719
YSRKEN <ysr.ken@gmail.com>
2019-02-27 22:08:20 +0900

    InputTalkFormのインデントを一弾減らした

--- imas-talk-maker/src/InputTalkForm.tsx
+++ imas-talk-maker/src/InputTalkForm.tsx
@@ -18,43 +18,41 @@ const derepoForm = (talkType: TalkType) => (talkType === 'デレぽ' ? (
 const isAutoInputName = (charaNameType: CharaNameType) => (charaNameType === '自動');
 
 // 入力フォーム全体
-const InputTalkForm: React.FC<{ className?: string }> = ({className = ""}) => {
-	return (
-		<ConfigContext.Consumer>
-			{(props) => {
-				if (props === null){
-					return (<></>);
-				}
+const InputTalkForm: React.FC<{ className?: string }> = ({className = ""}) => (
+	<ConfigContext.Consumer>
+		{(props) => {
+			if (props === null){
+				return (<></>);
+			}
 
-				// キャラ名が変更した際の処理
-				const onChangeCharaName = (event: React.FormEvent<ReplaceProps<"input", BsPrefixProps<"input">>>) => {
-					const value = event.currentTarget.value;
-					if (typeof value === 'string') {
-						props.setCharaName(value);
-					}
+			// キャラ名が変更した際の処理
+			const onChangeCharaName = (event: React.FormEvent<ReplaceProps<"input", BsPrefixProps<"input">>>) => {
+				const value = event.currentTarget.value;
+				if (typeof value === 'string') {
+					props.setCharaName(value);
 				}
+			}
 
-				return (
-					<Form className={`border p-3 ${className}`}>
-						<FormGroup>
-							<SelectButtonGroup className="w-100" nameList={TALK_TYPE_LIST}
-								firstSelectName={props.talkType} selectedColorType='primary'
-								callback={props.setTalkType}/>
-						</FormGroup>
-						<FormGroup>
-							<FormLabel>キャラ名</FormLabel>
-							<SelectButtonGroup className="ml-3 my-3" nameList={CHARA_NAME_TYPE_LIST}
-								firstSelectName={props.charaNameType} selectedColorType='primary'
-								callback={props.setCharaNameType}/>
-							<FormControl disabled={isAutoInputName(props.charaNameType)}
-								value={props.charaName} onChange={onChangeCharaName}/>
-						</FormGroup>
-						{derepoForm(props.talkType)}
-					</Form>
-				);
-			}}
-		</ConfigContext.Consumer>
-	);
-}
+			return (
+				<Form className={`border p-3 ${className}`}>
+					<FormGroup>
+						<SelectButtonGroup className="w-100" nameList={TALK_TYPE_LIST}
+							firstSelectName={props.talkType} selectedColorType='primary'
+							callback={props.setTalkType}/>
+					</FormGroup>
+					<FormGroup>
+						<FormLabel>キャラ名</FormLabel>
+						<SelectButtonGroup className="ml-3 my-3" nameList={CHARA_NAME_TYPE_LIST}
+							firstSelectName={props.charaNameType} selectedColorType='primary'
+							callback={props.setCharaNameType}/>
+						<FormControl disabled={isAutoInputName(props.charaNameType)}
+							value={props.charaName} onChange={onChangeCharaName}/>
+					</FormGroup>
+					{derepoForm(props.talkType)}
+				</Form>
+			);
+		}}
+	</ConfigContext.Consumer>
+);
 
 export default InputTalkForm;
