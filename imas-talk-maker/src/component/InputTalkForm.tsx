eeb59a4e750d696febb1787efc41e8ef7651af93
YSRKEN <ysr.ken@gmail.com>
2019-03-01 00:10:46 +0900

    InputTalkFormについて、一部の要素を別のReact.FCに切り出した

--- imas-talk-maker/src/component/InputTalkForm.tsx
+++ imas-talk-maker/src/component/InputTalkForm.tsx
@@ -4,11 +4,33 @@ import Form from 'react-bootstrap/FormGroup';
 import { BsPrefixProps, ReplaceProps } from 'react-bootstrap/helpers';
 import { findIconListByName } from 'src/iconData';
 import { CHARA_NAME_TYPE_LIST, CharaNameType, TALK_TYPE_LIST, TalkType } from '../constant';
-import { ConfigContext } from '../context';
+import { ConfigContext, IConfig } from '../context';
 import SelectButtonGroup from './SelectButtonGroup';
 
 // 自動入力時はtrue
 const isAutoInputName = (charaNameType: CharaNameType) => (charaNameType === '自動');
+
+// キャラ名入力
+const InputCharacterName: React.FC<{props: IConfig}> = ({props}) => {
+	// キャラ名が変更した際の処理
+	const onChangeCharaName = (event: React.FormEvent<ReplaceProps<"input", BsPrefixProps<"input">>>) => {
+		const value = event.currentTarget.value;
+		if (typeof value === 'string') {
+			props.setCharaName(value);
+		}
+	}
+
+	return (
+		<FormGroup>
+			<FormLabel>キャラ名</FormLabel>
+			<SelectButtonGroup className="ml-3 my-3" nameList={CHARA_NAME_TYPE_LIST}
+				firstSelectName={props.charaNameType} selectedColorType='secondary'
+				callback={props.setCharaNameType}/>
+			<FormControl disabled={isAutoInputName(props.charaNameType)}
+				value={props.charaName} onChange={onChangeCharaName}/>
+		</FormGroup>
+	);
+}
 
 // 表情セレクター
 const IconSelector: React.FC<{ className?: string, iconName: string }> = ({className = "", iconName = ""}) => {
@@ -45,13 +67,52 @@ const IconSelector: React.FC<{ className?: string, iconName: string }> = ({class
 	);
 }
 
-// デレぽモード時のみ表示されるフォーム
-const derepoForm = (talkType: TalkType) => (talkType === 'デレぽ' ? (
-	<FormGroup>
-		<FormLabel>ファボ数</FormLabel>
-		<FormControl />
+// キャラアイコン選択
+const InputCharacterIcon: React.FC<{props: IConfig}> = ({props}) => {
+	// キャラボタンを押した際の処理
+	const clickCharaButtonFunc = () => {
+		props.setViewType('SelectName');
+	}
+
+	// 表情ボタンを押した際の処理
+	const clickIconButtonFunc = () => {
+		if (props.iconSelectorFlg) {
+			props.setIconSelectorFlg(false);
+		} else {
+			props.setIconSelectorFlg(true);
+		}
+	}
+
+	return (
+		<FormGroup>
+			<FormLabel>アイコン({props.iconName})</FormLabel><br />
+			<div className="d-flex ml-3">
+				<img src={`${process.env.PUBLIC_URL}${props.iconURL}`} width="72" height="72" />
+				<div className="my-auto">
+					<Button className="mx-3" variant="secondary" onClick={clickCharaButtonFunc}>キャラ</Button>
+					<Button variant="secondary" onClick={clickIconButtonFunc}>表情</Button>
+				</div>
+			</div>
+			{props.iconSelectorFlg
+				? (<IconSelector iconName={props.iconName}/>)
+				:	(<></>)}
 	</FormGroup>
-) : (<></>));
+	);
+}
+
+// デレぽモード時のみ表示されるフォーム
+const DerepoForm: React.FC<{talkType: TalkType}> = ({talkType}) => {
+	if (talkType !== 'デレぽ') {
+		return (<></>);
+	}
+
+	return (
+		<FormGroup>
+			<FormLabel>ファボ数</FormLabel>
+			<FormControl />
+		</FormGroup>
+	);
+}
 
 // 入力フォーム全体
 const InputTalkForm: React.FC<{ className?: string }> = ({className = ""}) => (
@@ -59,28 +120,6 @@ const InputTalkForm: React.FC<{ className?: string }> = ({className = ""}) => (
 		{(props) => {
 			if (props === null){
 				return (<></>);
-			}
-
-			// キャラ名が変更した際の処理
-			const onChangeCharaName = (event: React.FormEvent<ReplaceProps<"input", BsPrefixProps<"input">>>) => {
-				const value = event.currentTarget.value;
-				if (typeof value === 'string') {
-					props.setCharaName(value);
-				}
-			}
-
-			// キャラボタンを押した際の処理
-			const clickCharaButtonFunc = () => {
-				props.setViewType('SelectName');
-			}
-
-			// 表情ボタンを押した際の処理
-			const clickIconButtonFunc = () => {
-				if (props.iconSelectorFlg) {
-					props.setIconSelectorFlg(false);
-				} else {
-					props.setIconSelectorFlg(true);
-				}
 			}
 
 			// 本文が変更した際の処理
@@ -98,32 +137,13 @@ const InputTalkForm: React.FC<{ className?: string }> = ({className = ""}) => (
 							firstSelectName={props.talkType} selectedColorType='primary'
 							callback={props.setTalkType}/>
 					</FormGroup>
-					<FormGroup>
-						<FormLabel>キャラ名</FormLabel>
-						<SelectButtonGroup className="ml-3 my-3" nameList={CHARA_NAME_TYPE_LIST}
-							firstSelectName={props.charaNameType} selectedColorType='secondary'
-							callback={props.setCharaNameType}/>
-						<FormControl disabled={isAutoInputName(props.charaNameType)}
-							value={props.charaName} onChange={onChangeCharaName}/>
-					</FormGroup>
-					<FormGroup>
-						<FormLabel>アイコン({props.iconName})</FormLabel><br />
-						<div className="d-flex ml-3">
-							<img src={`${process.env.PUBLIC_URL}${props.iconURL}`} width="72" height="72" />
-							<div className="my-auto">
-								<Button className="mx-3" variant="secondary" onClick={clickCharaButtonFunc}>キャラ</Button>
-								<Button variant="secondary" onClick={clickIconButtonFunc}>表情</Button>
-							</div>
-						</div>
-						{props.iconSelectorFlg
-							? (<IconSelector iconName={props.iconName}/>)
-							:	(<></>)}
-					</FormGroup>
+					<InputCharacterName props={props}/>
+					<InputCharacterIcon props={props}/>
 					<FormGroup>
 						<FormLabel>喋る内容</FormLabel>
 						<FormControl as='textarea' value={props.message} onChange={onChangeMessage} />
 					</FormGroup>
-					{derepoForm(props.talkType)}
+					<DerepoForm talkType={props.talkType}/>
 				</Form>
 			);
 		}}
