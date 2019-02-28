e87f38b89cdbe6251e955f82676aeb1751a30902
YSRKEN <ysr.ken@gmail.com>
2019-03-01 00:31:17 +0900

    SelectNameFormについても、一部を切り出して書き直した

--- imas-talk-maker/src/component/SelectNameForm.tsx
+++ imas-talk-maker/src/component/SelectNameForm.tsx
@@ -5,10 +5,45 @@ import { BsPrefixProps, ReplaceProps } from 'react-bootstrap/helpers';
 import { ConfigContext } from 'src/context';
 import { findIdolListBySearchWord } from 'src/iconData';
 
+interface IIdolInfo {
+  name: string
+  kana: string
+  url: string
+}
+
+// アイドル名の一覧表示
+const IdolNameList: React.FC<{idolList: IIdolInfo[]}> = ({idolList}) => {
+  const config = React.useContext(ConfigContext);
+  if (config === null) {
+    return (<></>);
+  }
+
+  return (
+    <ButtonGroup className="w-100" vertical={true}>
+      {idolList.map((idol, i) => {
+        const onClickButton = () => {
+          config.setIconName(idol.name);
+          config.setIconURL(idol.url);
+          config.setIconSelectorFlg(false);
+          config.setViewType('InputTalk');
+        }
+        return (<Button className="w-100" variant="outline-secondary" key={i}
+          onClick={onClickButton}>{idol.name}</Button>);
+      })}
+  </ButtonGroup>
+  );
+}
+
+// アイドルを検索して選択させるフォーム
 const SelectNameForm: React.FC<{ className?: string }> = ({className = ""}) => {
   const config = React.useContext(ConfigContext);
   if (config === null) {
     return (<></>);
+  }
+
+  // 戻るボタンを押した際の処理
+  const onClickBackButton = () => {
+    config.setViewType('InputTalk');
   }
 
   // 検索ワードが変化した際の処理
@@ -19,9 +54,9 @@ const SelectNameForm: React.FC<{ className?: string }> = ({className = ""}) => {
     }
   }
 
+  // Hooks
   const [searchWord, setSearchWord] = React.useState('');
-  const [idolList, setIdolList] = React.useState<Array<{name: string, kana: string, url: string}>>([]);
-
+  const [idolList, setIdolList] = React.useState<IIdolInfo[]>([]);
   React.useEffect(() => {
     findIdolListBySearchWord(searchWord).then((list) => {
 			setIdolList(list);
@@ -30,22 +65,12 @@ const SelectNameForm: React.FC<{ className?: string }> = ({className = ""}) => {
 
   return (
     <Form className={className}>
+      <Button className="w-100 my-3" variant="primary" onClick={onClickBackButton}>メイン画面に戻る</Button>
       <FormGroup>
         <FormLabel>キャラ名</FormLabel>
         <FormControl value={searchWord} onChange={onChangeSearchWord}/>
       </FormGroup>
-      <ButtonGroup className="w-100" vertical={true}>
-        {idolList.map((idol, i) => {
-          const onClickButton = () => {
-            config.setIconName(idol.name);
-            config.setIconURL(idol.url);
-            config.setIconSelectorFlg(false);
-            config.setViewType('InputTalk');
-          }
-          return (<Button className="w-100" variant="outline-secondary" key={i}
-            onClick={onClickButton}>{idol.name}</Button>);
-        })}
-      </ButtonGroup>
+      <IdolNameList idolList={idolList}/>
     </Form>
   );
 };
