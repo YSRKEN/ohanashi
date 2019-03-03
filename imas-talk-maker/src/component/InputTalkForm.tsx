cfd295eb06962122fbb4732611ebada869ef12aa
YSRKEN <ysr.ken@gmail.com>
2019-03-03 13:14:55 +0900

    タイトル部分の表示を付けた。また、プレビュー欄のデレぽ側で複数並べていたのをやめた。

--- imas-talk-maker/src/component/InputTalkForm.tsx
+++ imas-talk-maker/src/component/InputTalkForm.tsx
@@ -41,34 +41,45 @@ const InputTalkForm: React.FC<{ className?: string }> = ({className = ""}) => {
 	}
 
 	return (
-		<Form className={`border p-3 ${className}`}>
-			<FormGroup>
-				<SelectButtonGroup className="w-100" nameList={TALK_TYPE_LIST}
-					firstSelectName={config.talkType} selectedColorType='primary'
-					callback={config.setTalkType}/>
-			</FormGroup>
-			<InputCharacterName/>
-			<InputCharacterIcon/>
-			<FormGroup>
-				<FormLabel>喋る内容</FormLabel>
-				<FormControl as='textarea' value={config.message} onChange={onChangeMessage} />
-			</FormGroup>
-			<DerepoForm talkType={config.talkType}/>
-			<FormGroup>
-				<FormLabel>プレビュー</FormLabel>
-				{
-					config.talkType === 'おはなし'
-						? <div className="talk-list">
-							<OhanashiView talkData={previewData()}/>
-						</div>
-						: <div className="derepo-list">
-							<DerepoView talkData={previewData()} firstFlg={true}/>
-							<DerepoView talkData={previewData()} firstFlg={false}/>
-							<DerepoView talkData={previewData()} firstFlg={false}/>
-						</div>
-				}
-			</FormGroup>
-		</Form>
+		<>
+			<h1 className="text-center d-none d-sm-block my-3">アイマス会話メーカー</h1>
+			<h2 className="text-center d-xs-block d-sm-none my-3">アイマス会話メーカー</h2>
+			<p className="text-center">
+				<a href="https://github.com/YSRKEN/ohanashi">GitHub</a>
+				<span className="ml-3">
+					作者：
+					<a href="https://twitter.com/YSRKEN" className="mr-3">
+						@YSRKEN
+					</a>
+				</span>
+			</p>
+			<Form className={`border p-3 ${className}`}>
+				<FormGroup>
+					<SelectButtonGroup className="w-100" nameList={TALK_TYPE_LIST}
+						firstSelectName={config.talkType} selectedColorType='primary'
+						callback={config.setTalkType}/>
+				</FormGroup>
+				<InputCharacterName/>
+				<InputCharacterIcon/>
+				<FormGroup>
+					<FormLabel>喋る内容</FormLabel>
+					<FormControl as='textarea' value={config.message} onChange={onChangeMessage} />
+				</FormGroup>
+				<DerepoForm talkType={config.talkType}/>
+				<FormGroup>
+					<FormLabel>プレビュー</FormLabel>
+					{
+						config.talkType === 'おはなし'
+							? <div className="talk-list">
+								<OhanashiView talkData={previewData()}/>
+							</div>
+							: <div className="derepo-list">
+								<DerepoView talkData={previewData()} firstFlg={true}/>
+							</div>
+					}
+				</FormGroup>
+			</Form>
+		</>
 	);
 }
 
