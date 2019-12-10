ad34e16045387917c163bc68a461f99e4a52263f
YSRKEN <ysr.ken@gmail.com>
2019-12-10 14:05:38 +0900

    [add] 人物名およびメッセージを表示するようにした

--- imas-talk-maker-v3/src/component/ohanashi-view.tsx
+++ imas-talk-maker-v3/src/component/ohanashi-view.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { OhanashiData } from 'constant';
+import { OhanashiData, APPLICATION_FONT } from 'constant';
 import { loadImage } from 'service/utility';
 
 // 「おはなし」の大きさ
@@ -8,10 +8,7 @@ const OHANASHI_HEIGHT = 84;
 const ICON_SIZE = 76;
 
 // 描画メソッド
-const drawMethodImpl = async (
-  canvas: CanvasRenderingContext2D,
-  dataList: OhanashiData[]
-) => {
+const drawMethodImpl = async (canvas: CanvasRenderingContext2D, dataList: OhanashiData[]) => {
   // 描画用の画像を読み込む
   const bgImage = await loadImage('./asset/background.png');
   const balloon1 = await loadImage('./asset/balloon1.png');
@@ -21,58 +18,18 @@ const drawMethodImpl = async (
   // 描画を行う
   for (let di = 0; di < dataList.length; di += 1) {
     // 背景の描画
-    canvas.drawImage(
-      bgImage,
-      0,
-      0,
-      OHANASHI_WIDTH,
-      OHANASHI_HEIGHT,
-      0,
-      OHANASHI_HEIGHT * di,
-      OHANASHI_WIDTH,
-      OHANASHI_HEIGHT
-    );
+    canvas.drawImage(bgImage, 0, 0, OHANASHI_WIDTH, OHANASHI_HEIGHT, 0, OHANASHI_HEIGHT * di, OHANASHI_WIDTH, OHANASHI_HEIGHT);
 
     // 会話枠の描画
     switch (dataList[di].messageMode) {
       case 'normal':
-        canvas.drawImage(
-          balloon1,
-          0,
-          0,
-          OHANASHI_WIDTH,
-          OHANASHI_HEIGHT,
-          0,
-          OHANASHI_HEIGHT * di,
-          OHANASHI_WIDTH,
-          OHANASHI_HEIGHT
-        );
+        canvas.drawImage(balloon1, 0, 0, OHANASHI_WIDTH, OHANASHI_HEIGHT, 0, OHANASHI_HEIGHT * di, OHANASHI_WIDTH, OHANASHI_HEIGHT);
         break;
       case 'double':
-        canvas.drawImage(
-          balloon2,
-          0,
-          0,
-          OHANASHI_WIDTH,
-          OHANASHI_HEIGHT,
-          0,
-          OHANASHI_HEIGHT * di,
-          OHANASHI_WIDTH,
-          OHANASHI_HEIGHT
-        );
+        canvas.drawImage(balloon2, 0, 0, OHANASHI_WIDTH, OHANASHI_HEIGHT, 0, OHANASHI_HEIGHT * di, OHANASHI_WIDTH, OHANASHI_HEIGHT);
         break;
       case 'message-only':
-        canvas.drawImage(
-          balloon3,
-          0,
-          0,
-          OHANASHI_WIDTH,
-          OHANASHI_HEIGHT,
-          0,
-          OHANASHI_HEIGHT * di,
-          OHANASHI_WIDTH,
-          OHANASHI_HEIGHT
-        );
+        canvas.drawImage(balloon3, 0, 0, OHANASHI_WIDTH, OHANASHI_HEIGHT, 0, OHANASHI_HEIGHT * di, OHANASHI_WIDTH, OHANASHI_HEIGHT);
         break;
       default:
         // dataList[di].messageMode === 'quartet'の場合、会話枠は描画しない
@@ -82,74 +39,72 @@ const drawMethodImpl = async (
     // アイコンの描画
     switch (dataList[di].messageMode) {
       case 'normal': {
-        const iconImage = await loadImage(
-          `./asset/${dataList[di].iconUrls[0]}`
-        );
-        canvas.drawImage(
-          iconImage,
-          0,
-          0,
-          iconImage.width,
-          iconImage.height,
-          5,
-          OHANASHI_HEIGHT * di + 4,
-          ICON_SIZE,
-          ICON_SIZE
-        );
+        const iconImage = await loadImage(`./asset/${dataList[di].iconUrls[0]}`);
+        canvas.drawImage(iconImage, 0, 0, iconImage.width, iconImage.height, 5, OHANASHI_HEIGHT * di + 4, ICON_SIZE, ICON_SIZE);
         break;
       }
       case 'double': {
-        const iconImage1 = await loadImage(
-          `./asset/${dataList[di].iconUrls[0]}`
-        );
-        const iconImage2 = await loadImage(
-          `./asset/${dataList[di].iconUrls[1]}`
-        );
-        canvas.drawImage(
-          iconImage1,
-          0,
-          0,
-          iconImage1.width,
-          iconImage1.height,
-          5,
-          OHANASHI_HEIGHT * di + 4,
-          ICON_SIZE,
-          ICON_SIZE
-        );
-        canvas.drawImage(
-          iconImage2,
-          0,
-          0,
-          iconImage2.width,
-          iconImage2.height,
-          OHANASHI_WIDTH - 5 - ICON_SIZE,
-          OHANASHI_HEIGHT * di + 4,
-          ICON_SIZE,
-          ICON_SIZE
-        );
+        const iconImage1 = await loadImage(`./asset/${dataList[di].iconUrls[0]}`);
+        const iconImage2 = await loadImage(`./asset/${dataList[di].iconUrls[1]}`);
+        canvas.drawImage(iconImage1, 0, 0, iconImage1.width, iconImage1.height, 5, OHANASHI_HEIGHT * di + 4, ICON_SIZE, ICON_SIZE);
+        canvas.drawImage(iconImage2, 0, 0, iconImage2.width, iconImage2.height, OHANASHI_WIDTH - 5 - ICON_SIZE, OHANASHI_HEIGHT * di + 4, ICON_SIZE, ICON_SIZE);
         break;
       }
       case 'quartet': {
         for (let ii = 0; ii < 4; ii += 1) {
-          const iconImage = await loadImage(
-            `./asset/${dataList[di].iconUrls[ii]}`
-          );
-          canvas.drawImage(
-            iconImage,
-            0,
-            0,
-            iconImage.width,
-            iconImage.height,
-            3 + ICON_SIZE * ii + 3 * ii,
-            OHANASHI_HEIGHT * di + 4,
-            ICON_SIZE,
-            ICON_SIZE
-          );
+          const iconImage = await loadImage(`./asset/${dataList[di].iconUrls[ii]}`);
+          canvas.drawImage(iconImage, 0, 0, iconImage.width, iconImage.height, 3 + ICON_SIZE * ii + 3 * ii, OHANASHI_HEIGHT * di + 4, ICON_SIZE, ICON_SIZE);
         }
         break;
       }
       default:
         // dataList[di].messageMode === 'message-only'の場合、アイコンは描画しない
+        break;
+    }
+
+    // テキストの描画
+    switch (dataList[di].messageMode) {
+      case 'normal':
+        // 準備
+        canvas.font = `14px ${APPLICATION_FONT}`;
+        canvas.textBaseline = 'top';
+
+        // 名前部分を描画
+        canvas.fillStyle = '#f33281';
+        canvas.fillText(dataList[di].name, ICON_SIZE + 16, OHANASHI_HEIGHT * di + 8, 214);
+
+        // メッセージ部分を描画
+        canvas.fillStyle = 'black';
+        canvas.fillText(dataList[di].message, ICON_SIZE + 16, OHANASHI_HEIGHT * di + 23, 214);
+        break;
+      case 'double':
+        // 準備
+        canvas.font = `14px ${APPLICATION_FONT}`;
+        canvas.textBaseline = 'top';
+
+        // 名前部分を描画
+        canvas.fillStyle = '#f33281';
+        canvas.fillText(dataList[di].name, ICON_SIZE + 16, OHANASHI_HEIGHT * di + 8, 140);
+
+        // メッセージ部分を描画
+        canvas.fillStyle = 'black';
+        canvas.fillText(dataList[di].message, ICON_SIZE + 16, OHANASHI_HEIGHT * di + 23, 140);
+        break;
+      case 'message-only':
+        // 準備
+        canvas.font = `14px ${APPLICATION_FONT}`;
+        canvas.textBaseline = 'top';
+
+        // 名前部分を描画
+        canvas.fillStyle = '#f33281';
+        canvas.fillText(dataList[di].name, 14, OHANASHI_HEIGHT * di + 8, 292);
+
+        // メッセージ部分を描画
+        canvas.fillStyle = 'black';
+        canvas.fillText(dataList[di].message, 14, OHANASHI_HEIGHT * di + 23, 292);
+        break;
+      default:
+        // dataList[di].messageMode === 'quartet'の場合、テキストは描画しない
         break;
     }
   }
@@ -177,13 +132,7 @@ const OhanashiView: React.FC<{ dataList: OhanashiData[] }> = ({ dataList }) => {
     drawMethod();
   }, [canvasRef]);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      width={OHANASHI_WIDTH}
-      height={OHANASHI_HEIGHT * dataList.length}
-    />
-  );
+  return <canvas ref={canvasRef} width={OHANASHI_WIDTH} height={OHANASHI_HEIGHT * dataList.length} />;
 };
 
 export default OhanashiView;
--- imas-talk-maker-v3/src/constant.ts
+++ imas-talk-maker-v3/src/constant.ts
@@ -1,11 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
+// システムで使用するフォント
+export const APPLICATION_FONT = 'Noto Sans JP';
+
+// システムで使用するフォントに対応するURL
+const FONT_URL = `https://fonts.googleapis.com/css?family=${APPLICATION_FONT.replace(' ', '+')}`;
+
 // システム共通に適用する CSS を設定する
 export const GlobalStyles = createGlobalStyle`
-  /* フォントを Noto Sans JP に設定する */
-  @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP');
+  /* フォントを指定した Web フォントに設定する */
+  @import url('${FONT_URL}');
   body {
-    font-family: 'Noto Sans JP', sans-serif;
+    font-family: '${APPLICATION_FONT}', sans-serif;
   }
 `;
 
@@ -36,26 +42,18 @@ export const SAMPLE_OHANASHI: OhanashiData[] = [
   },
   {
     name: '恵美＆朋花',
-    iconUrls: [
-      'million/tokoro_megumi-1.png',
-      'million/tenkubashi_tomoka-1.png'
-    ],
+    iconUrls: ['million/tokoro_megumi-1.png', 'million/tenkubashi_tomoka-1.png'],
     message: 'サンプルメッセージ3',
     messageMode: 'double'
   },
   {
     name: '',
-    iconUrls: [
-      'million/tenkubashi_tomoka-1.png',
-      'million/tokoro_megumi-1.png',
-      'million/momose_rio-1.png',
-      'million/nikaido_chizuru-1.png'
-    ],
+    iconUrls: ['million/tenkubashi_tomoka-1.png', 'million/tokoro_megumi-1.png', 'million/momose_rio-1.png', 'million/nikaido_chizuru-1.png'],
     message: '',
     messageMode: 'quartet'
   },
   {
-    name: '',
+    name: '状況説明',
     iconUrls: [],
     message: 'サンプルメッセージ4',
     messageMode: 'message-only'
