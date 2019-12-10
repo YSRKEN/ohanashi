222bebbc0fec37e1854101401c41a335dda319d9
YSRKEN <ysr.ken@gmail.com>
2019-12-10 15:12:26 +0900

    [add] 自動改行ルーチンを実装することで、折返しを気にせず書けるようになった

--- imas-talk-maker-v3/src/constant.ts
+++ imas-talk-maker-v3/src/constant.ts
@@ -31,19 +31,19 @@ export const SAMPLE_OHANASHI: OhanashiData[] = [
   {
     name: '千鶴',
     iconUrls: ['million/nikaido_chizuru-1.png'],
-    message: '目前の光景は実に壮大なものだっ\nた。',
+    message: '目前の光景は実に壮大なものだった。',
     messageMode: 'normal'
   },
   {
     name: '莉緒',
     iconUrls: ['million/momose_rio-1.png'],
-    message: '5 平方フィート程度の紫色の草むら\nのようなものが、砂地を横切ってこ\nちらに向かってきた。',
+    message: '5 平方フィート程度の紫色の草むらのようなものが、砂地を横切ってこちらに向かってきた。',
     messageMode: 'normal'
   },
   {
     name: '恵美＆朋花',
     iconUrls: ['million/tokoro_megumi-1.png', 'million/tenkubashi_tomoka-1.png'],
-    message: '赤い炎が尖った翼の\n輪郭を浮かび上がら\nせた。',
+    message: '赤い炎が尖った翼の輪郭を浮かび上がらせた。',
     messageMode: 'double'
   },
   {
@@ -55,7 +55,7 @@ export const SAMPLE_OHANASHI: OhanashiData[] = [
   {
     name: '状況説明',
     iconUrls: [],
-    message: '各部位を正確に作るには時間がかかるので、\n当初の意図とは異なるが、巨大な人体を作る\nことにした。高さは約 8 フィートで、これに',
+    message: '各部位を正確に作るには時間がかかるので、当初の意図とは異なるが、巨大な人体を作ることにした。高さは約 8 フィートで、これに',
     messageMode: 'message-only'
   }
 ];
